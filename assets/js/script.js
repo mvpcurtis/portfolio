1332a67adaedd310e0c151d561da01ac824cd590
mvpcurtis <curtis.bowen@gmx.com>
2023-11-26 20:06:34 +0000

    implemented theme toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ window.addEventListener("scroll", function () {
 	navigation.classList.toggle("sticky", window.scrollY > 0);
 	console.log("Scroll bar active!");
 });
+
+// DARK/LIGHT MODE TOGGLE
+var toggleTheme = document.getElementById("toggle-theme");
+
+toggleTheme.onclick = function () {
+	document.body.classList.toggle("dark-theme");
+	console.log("Theme toggled");
+};
