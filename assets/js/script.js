71b3900b8c3d52f87f692cc0ade4c51dce65ea6a
mvpcurtis <curtis.bowen@gmx.com>
2023-11-26 15:21:01 +0000

    added programming for sticky navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
-console.log("Hello World!");
+window.addEventListener("scroll", function () {
+	var navigation = document.querySelector("nav");
+	navigation.classList.toggle("sticky", window.scrollY > 0);
+});
